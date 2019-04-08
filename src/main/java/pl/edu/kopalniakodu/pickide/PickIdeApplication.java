c4f84dfd8fbc26cf402607574ab1c9d6ed9caa07
Szymon Gąsior <szgasior@outlook.com>
2019-04-08 14:46:38 +0200

    Initial commit

---
+++ src/main/java/pl/edu/kopalniakodu/pickide/PickIdeApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.kopalniakodu.pickide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickIdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickIdeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/kopalniakodu/pickide/PickIdeApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.kopalniakodu.pickide;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PickIdeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
